3d0943b2c4af4170c6aef94bc0bde3867b8b881e
Scott Solmonson <ssolmonson63@gmail.com>
2020-08-25 23:53:01 -0400

    Modify restaurant model and controller
    Made changes to the restaurant controller and model, and verified the
    serializer was set correctly. This includes changing the controller to
    be protected.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  belongs_to :user
+  validates :city,
+            :state,
+            :address,
+            :foodtype,
+            presence: true
 end
--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,3 +1,3 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id, :city, :state, :address, :foodtype
+  attributes :id, :city, :state, :address, :foodtype, :user_id
 end
