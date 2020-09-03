ea806891f427fd2c12f992e34fc709b257efc1ae
Scott Solmonson <ssolmonson63@gmail.com>
2020-09-03 05:53:53 -0400

    Add columns to restaurants table
    1. Added user key to restaurants.
    2. Added a 'zipcode' column to restaurants for smoother locating.

---
+++ db/migrate/20200903095002_add_zipcode_to_restaurants.rb
@@ -0,0 +1,5 @@
+class AddZipcodeToRestaurants < ActiveRecord::Migration[5.2]
+  def change
+    add_column :restaurants, :zipcode, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 2020_09_03_095002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 2) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_examples_on_user_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "city"
+    t.string "state"
+    t.string "address"
+    t.string "foodtype"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.integer "zipcode"
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +46,5 @@ ActiveRecord::Schema.define(version: 2) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "restaurants", "users"
 end
